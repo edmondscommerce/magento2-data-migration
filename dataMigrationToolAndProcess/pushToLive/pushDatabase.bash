95698a2c12e14b7054a95e3674ff758d7d2fafbc
Tadas Simkus <tadas@edmondscommerce.co.uk>
2017-10-26 11:32:20 +0000

    created pushDatabase

---
+++ dataMigrationToolAndProcess/pushToLive/pushDatabase.bash
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+cd $DIR;
+set -e
+set -u
+set -o pipefail
+standardIFS="$IFS"
+IFS=$'\n\t'
+echo "
+===========================================
+$(hostname) $0 $@
+===========================================
+"
+
+function usage(){
+    echo "
+
+Usage:
+    ./$0 [localDbUserName] [localDbPassword] [localDbHost] [localDbName] [sshHost] (sshPort=2020)
+
+    localDbUserName - Local db username
+    localDbPassword - Local db password
+    localDbHost     - Local host
+    sshHost         - The SSH host
+    remoteMediaPath - The remote public path containing the Magento 2 media directory
+    remoteOwner     - User by which web server is running
+    sshPort         - The SSH port number
+    "
+}
+
+
+if (( $# < 5 ))
+then
+    usage
+    exit 1
+fi
+
+localDbUserName=$1
+localDbPassword=$2
+localDbHost=$3
+localDbName=$4
+sshHost=$5
+sshPort=${6:-2020}
+
+
+echo "Making sure we have pv installed"
+if [[ "" = "$(command -v pv)" ]]
+then
+    sudo yum -y install pv
+fi
+
+echo "
+Dumping, compressing, piping over SSH and decompressing and inserting on remote
+.. all in one big beautiful pipe
+"
+mysqldump \
+    --default-character-set=utf8 \
+    --single-transaction \
+    --add-drop-database \
+    --add-drop-table \
+    -u ${localDbUserName} \
+    -p${localDbPassword} \
+    -h ${localDbHost} \
+    $localDbName  \
+    | gzip -c \
+    | pv \
+    | ssh "root@$sshHost" -p $sshPort \
+        -- bash -c "
+        zcat -d | mysql $localDbName
+    "
---
+++ dataMigrationToolAndProcess/pushToLive/pushMedia.bash
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+readonly DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+cd $DIR;
+set -e
+set -u
+set -o pipefail
+standardIFS="$IFS"
+IFS=$'\n\t'
+echo "
+===========================================
+$(hostname) $0 $@
+===========================================
+"
+
+
+function usage(){
+    echo "
+
+Usage:
+    ./$0 [sshHost] [remoteMediaPath]  (sshPort=2020)
+
+    sshHost         - The SSH host
+    remoteMediaPath - The remote public path containing the Magento 2 media directory
+    remoteOwner     - User by which web server is running
+    sshPort         - The SSH port number
+    "
+}
+
+
+if (( $# < 3 ))
+then
+    usage
+    exit 1
+fi
+
+### Paramaters ###
+sshHost="$1"
+remoteMediaPath="$2/"
+remoteOwner="$3"
+sshPort="${4:-2020}"
+localMediaPath="./../../../../../pub/media/"
+
+
+echo "
+Setting up SSH ID
+"
+set +e
+"ssh -p $sshPort root@$sshHost -oBatchMode=yes exit" &> /dev/null
+if (( $? != 0 ))
+then
+    ssh-copy-id  -o StrictHostKeyChecking=no root@$sshHost -p $sshPort
+fi
+set -e
+
+
+
+echo "$localMediaPath --> $remoteMediaPath";
+rsync -vz \
+    --omit-dir-times \
+    --exclude '*/cache/*' \
+    --exclude '*.gz' \
+    -e "ssh -p $sshPort" \
+    $localMediaPath \
+    "root@$sshHost":"$remoteMediaPath"
+
+echo "Done!";
+
+echo "Logging in to remote and chaging file and directories permissions";
+
+echo "
+echo \"Changed directory to $remoteMediaPath\"
+cd $remoteMediaPath;
+echo \"Giving ownership and permissions to $remoteMediaPath\"
+chown $remoteOwner:$remoteOwner -R .
+find . -type f -exec chmod 664 {} \;
+find . -type d -exec chmod 775 {} \;
+echo \"All done!\"
+" | ssh "root@$sshHost" -p $sshPort;
